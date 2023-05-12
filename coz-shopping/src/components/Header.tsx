c4b55215d34d03a085dc815be8ca38444d6f5da1
jasmine0714 <woooooooow22@gmail.com>
2023-05-12 17:37:17 +0900

    🎨 모달창 구현 (React modal) 이용

--- coz-shopping/src/components/Header.tsx
+++ coz-shopping/src/components/Header.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+
+const modalStyle: ReactModal.Styles = {
+  content: {
+    top: "17%",
+    left: "91%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "200px",
+    height: "200px",
+    position: "absolute",
+  },
+};
 
 const HeaderWrapper = styled.header`
   border: 1px solid transparent;
@@ -28,11 +44,33 @@ const Hamburger = styled.img`
 `;
 
 function Header() {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsDropDownOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsDropDownOpen(false);
+  };
+
   return (
     <HeaderWrapper>
       <CodeStatesLogo src='./image/로고.png'></CodeStatesLogo>
       <COZShopping src='./image/쇼핑몰 이름.jpg'></COZShopping>
-      <Hamburger src='./image/hamburger.jpg'></Hamburger>
+      <Hamburger
+        src='./image/hamburger.jpg'
+        onClick={handleOpenModal}
+      ></Hamburger>
+      <Modal
+        isOpen={isDropDownOpen}
+        onRequestClose={handleCloseModal}
+        style={modalStyle}
+      >
+        <div>OOO님, 안녕하세요!</div>
+        <div>상품리스트 페이지</div>
+        <div>북마크 페이지</div>
+      </Modal>
     </HeaderWrapper>
   );
 }
