fdf3cd5d8ec8ef18c284430b7c518b5c4847fc6f
jasmine0714 <woooooooow22@gmail.com>
2023-05-12 18:05:49 +0900

    🎨 Main, Footer 컴포넌트 생성

---
+++ coz-shopping/src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const FooterWrapper = styled.footer`
+  border: 1px solid transparent;
+  margin: none;
+  height: 50px;
+  box-shadow: 0px 5px 5px gray;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+const FooterContent = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 88.02%;
+  display: inline-block;
+  color: #888888;
+  text-align: center;
+  margin-bottom: -10px;
+  margin-top: 15px;
+`;
+
+function Footer() {
+  return (
+    <FooterWrapper>
+      <FooterContent>개인정보 처리방침 | 이용약관</FooterContent>
+      <FooterContent>All rights reserved @ CodeStates</FooterContent>
+    </FooterWrapper>
+  );
+}
+
+export default Footer;
---
+++ coz-shopping/src/components/Main.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const MainWrapper = styled.header`
+  border: 1px solid transparent;
+  margin: none;
+  height: 820px;
+  box-shadow: 0px 5px 5px gray;
+  display: flex;
+  flex-direction: row;
+`;
+
+function Main() {
+  return <MainWrapper></MainWrapper>;
+}
+export default Main;
