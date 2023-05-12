7025622aee5ff2eb659c74161265c3af7b71962d
jasmine0714 <woooooooow22@gmail.com>
2023-05-12 20:02:13 +0900

    🚚 Footer와 Header를 다시 Home 폴더 밖으로 이전

--- coz-shopping/src/App.tsx
+++ coz-shopping/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./components/Home/Header";
-import Footer from "./components/Home/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Main from "./components/Home/Main";
 
 function App() {
