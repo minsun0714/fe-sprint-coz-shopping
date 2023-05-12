a121adedd4e9a6d427dbe203d1b37df9dfa75ad9
jasmine0714 <woooooooow22@gmail.com>
2023-05-13 00:06:44 +0900

    ✨ products slice reducer 구축

---
+++ coz-shopping/src/store/productsStore.tsx
@@ -0,0 +1,17 @@
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IItem } from "../components/Home/Main";
+
+const initialProducts: IItem[] = [];
+
+export const products = createSlice({
+  name: "productsReducer",
+  initialState: initialProducts,
+  reducers: {
+    storeAllProducts: (state: IItem[], action: PayloadAction<IItem>) => {
+      return [...state, action.payload];
+    },
+  },
+});
+
+export const store = configureStore({ reducer: products.reducer });
+export const { storeAllProducts } = products.actions;
