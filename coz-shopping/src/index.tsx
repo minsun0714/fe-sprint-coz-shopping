4a2ab9ab6d9ee227eda132681c3b0eadd47bfc28
jasmine0714 <woooooooow22@gmail.com>
2023-05-15 10:17:47 +0900

    💄 GlobalStyle 적용하여 화면 마진 제거

--- coz-shopping/src/index.tsx
+++ coz-shopping/src/index.tsx
@@ -3,12 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./store/productsStore";
 import { Provider } from "react-redux";
+import { createGlobalStyle } from "styled-components";
+import normalize from "normalize.css";
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+header {
+  margin: 0;
+}
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <>
+    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>
 );
