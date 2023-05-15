1ac005deadc7fc3d310c81b85482494c0438cb9a
jasmine0714 <woooooooow22@gmail.com>
2023-05-15 19:52:50 +0900

    ✨ bookmark한 품목들만 볼 수 있는 page 구현

--- coz-shopping/src/components/BookMarkList/BookMarkList.tsx
+++ coz-shopping/src/components/BookMarkList/BookMarkList.tsx
@@ -1,13 +1,56 @@
 import { useState } from "react";
-import { BookMarkStar } from "../Home/Main";
-import FilterBtn from "../ProductList/FilterBtn";
+import { Item, ItemImg, ItemBox } from "../Home/Main";
+import FilterBookMarkBtn from "../BookMarkList/FilterBookMarkBtn";
 import ProductList, {
   ProductListMainWrapper,
+  BookMarkStar,
+  Section,
 } from "../ProductList/ProductList";
 import { IItem } from "../Home/Main";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store/rootStore";
+import {
+  getBookMarkedProducts,
+  deleteBookMarkedProduct,
+} from "../../store/bookMarkStore";
 
 function BookMarkList() {
-  return <ProductListMainWrapper></ProductListMainWrapper>;
+  const [items, setItems] = useState<IItem[]>([]);
+  const dispatch = useDispatch();
+  const products = useSelector((store: RootState) => store.products);
+  const bookMarkedProducts = useSelector(
+    (store: RootState) => store.bookMarkedProducts
+  );
+
+  const onClickBookMark = (id: number) => {
+    const bookMarkedTargetItem = bookMarkedProducts.find(
+      (product: IItem) => product.id === id
+    );
+
+    if (!bookMarkedTargetItem) {
+      const targetItem = products.find((product: IItem) => product.id === id);
+      if (targetItem) dispatch(getBookMarkedProducts(targetItem));
+    } else dispatch(deleteBookMarkedProduct(bookMarkedTargetItem));
+  };
+
+  return (
+    <ProductListMainWrapper>
+      <FilterBookMarkBtn setFilteredItems={setItems} />
+      <Section>
+        <ItemBox>
+          {items?.map((item: IItem) => (
+            <Item key={item.id}>
+              <ItemImg src={item.image_url || item.brand_image_url}></ItemImg>
+              <BookMarkStar
+                id={item.id}
+                onClick={() => onClickBookMark(item.id)}
+              ></BookMarkStar>
+              <li>{item.title || item.brand_name}</li>
+            </Item>
+          ))}
+        </ItemBox>
+      </Section>
+    </ProductListMainWrapper>
+  );
 }
 export default BookMarkList;
---
+++ coz-shopping/src/components/BookMarkList/FilterBookMarkBtn.tsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/rootStore";
+
+const ButtonsWrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  margin: 20px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledFilterBtn = styled.button`
+  border-radius: 100px;
+  height: 60px;
+  width: 60px;
+  border: none;
+  margin: 8px;
+`;
+
+interface IItem {
+  brand_image_url?: string;
+  brand_name: string | null;
+  discountPercentage: number | null;
+  follower: number;
+  id: number;
+  image_url: string | null;
+  price: string | null;
+  sub_title: string | null;
+  title: string | null;
+  type: string;
+}
+
+interface IFilteredBtn {
+  setFilteredItems: (items: IItem[]) => void;
+}
+
+function FilterBookMarkBtn({ setFilteredItems }: IFilteredBtn) {
+  const bookMarkStore = useSelector(
+    (store: RootState) => store.bookMarkedProducts
+  );
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const type = event.currentTarget.name;
+    console.log(type);
+    setFilteredItems(
+      bookMarkStore.filter((product: IItem) => product.type === type)
+    );
+  };
+  return (
+    <ButtonsWrapper>
+      <ButtonWrapper>
+        <StyledFilterBtn></StyledFilterBtn>
+        <label>전체</label>
+      </ButtonWrapper>
+      <ButtonWrapper>
+        <StyledFilterBtn
+          name='Product'
+          onClick={(e) => onClick(e)}
+        ></StyledFilterBtn>
+        <label>상품</label>
+      </ButtonWrapper>
+      <ButtonWrapper>
+        <StyledFilterBtn
+          name='Category'
+          onClick={(e) => onClick(e)}
+        ></StyledFilterBtn>
+        <label>카테고리</label>
+      </ButtonWrapper>
+      <ButtonWrapper>
+        <StyledFilterBtn
+          name='Exhibition'
+          onClick={(e) => onClick(e)}
+        ></StyledFilterBtn>
+        <label>기획전</label>
+      </ButtonWrapper>
+      <ButtonWrapper>
+        <StyledFilterBtn
+          name='Brand'
+          onClick={(e) => onClick(e)}
+        ></StyledFilterBtn>
+        <label>브랜드</label>
+      </ButtonWrapper>
+    </ButtonsWrapper>
+  );
+}
+export default FilterBookMarkBtn;
