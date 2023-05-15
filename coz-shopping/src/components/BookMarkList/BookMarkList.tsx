f5b2818b76b1bdbed61d6b18cad609767f83f5c0
jasmine0714 <woooooooow22@gmail.com>
2023-05-15 15:54:00 +0900

    💄  ProducListMainWrapper 적용

--- coz-shopping/src/components/BookMarkList/BookMarkList.tsx
+++ coz-shopping/src/components/BookMarkList/BookMarkList.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { BookMarkStar } from "../Home/Main";
+import FilterBtn from "../ProductList/FilterBtn";
+import ProductList, {
+  ProductListMainWrapper,
+} from "../ProductList/ProductList";
+import { IItem } from "../Home/Main";
+import { useSelector } from "react-redux";
 
 function BookMarkList() {
-  return null;
+  return <ProductListMainWrapper></ProductListMainWrapper>;
 }
 export default BookMarkList;
