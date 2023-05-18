65adaf2a4aa23d1ef9fc56d3e4674bc2bffb282b
jasmine0714 <woooooooow22@gmail.com>
2023-05-18 14:28:07 +0900

    ✨ bookmark 시 알림 토스트 기능 구현 (react-toastify 사용)

--- coz-shopping/src/GlobalStyle.tsx
+++ coz-shopping/src/GlobalStyle.tsx
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import normalize from "normalize.css";
+import styled from "styled-components";
+import { ToastContainer } from "react-toastify";
 
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
@@ -7,3 +9,31 @@ export const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
 }
 `;
+
+export const StyledToastContainer = styled(ToastContainer)`
+  &&&.Toastify__toast-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    width: 330px;
+    height: 480px;
+  }
+  .Toastify__toast {
+    background-color: transparent;
+    box-shadow: none;
+    margin-top: -25px;
+  }
+  .Toastify__toast-body {
+    background-color: transparent;
+  }
+  .Toastify__progress-bar {
+    border: 10px solid transparent;
+  }
+`;
+
+export const ToastImg = styled.img`
+  height: 65px;
+  margin-bottom: -20px;
+`;
+
+export type ToastAction = "add" | "delete";
