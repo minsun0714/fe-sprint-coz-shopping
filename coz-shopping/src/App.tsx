bebe5f4ccadc8b332df65e50c95ee1941e0a576d
jasmine0714 <woooooooow22@gmail.com>
2023-05-21 18:55:05 +0900

    🥅 import React

--- coz-shopping/src/App.tsx
+++ coz-shopping/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header/components/Header";
 import Footer from "./Footer/components/Footer";
